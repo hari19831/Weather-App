202909227293c3db0b99ae72715a3f02d9c0d347
hari19831 <hari.19831@gmail.com>
2017-05-04 16:40:11 +0530

    Wjs

---
+++ weather.js
@@ -0,0 +1,38 @@
+ 
+window.onload = function(){
+    //alert('hai');
+    document.getElementById('date').innerHTML=Date();
+};
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else { 
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+   alert("Latitude: " + position.coords.latitude + "  ||  "+
+    "Longitude: " + position.coords.longitude);
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.")
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert( "Location information is unavailable.")
+            break;
+        case error.TIMEOUT:
+           alert( "The request to get user location timed out.")
+            break;
+        case error.UNKNOWN_ERROR:
+          alert( "An unknown error occurred.")
+            break;
+    }
+}
+  
